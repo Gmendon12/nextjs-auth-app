7a60c7247d01c3b60485137276e314798c78a7e2
Gmendon12 <gmendon46@gmail.com>
2025-05-25 23:56:20 +0530

    first commit

---
+++ app/lib/firebase.js
@@ -0,0 +1,18 @@
+import {initializeApp} from 'firebase/app'
+import { getAuth, GoogleAuthProvider} from 'firebase/auth'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDlJ39bmgQmdzhf_bg3GfhHXIeoB7pE4BM",
+    authDomain : "auth-c69fd.firebaseapp.com",
+    projectId : "auth-c69fd",
+    storageBucket: "auth-c69fd.appspot.com",
+    messagingSenderId: "6386947447",
+    appId : "1:6386947447:web:4add2249a069b8c4953d72",
+    measurementId: "G-Q4YXNS7F5J"
+};
+
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+const provider = new GoogleAuthProvider()
+
+export {auth, provider}
